f96ca55b27e5d85162ac67017fb2b0d51e56f64e
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-05-03 12:27:10 +0000

    feat: Pruebas unitarias

---
+++ servicioCorreos/test/correos.js
@@ -0,0 +1,39 @@
+let expect = require('chai').expect;
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+
+describe('Validar Autenticacion de usuario', function () {
+    var dataEmail,dataEmailIncorrect;
+    beforeEach(function (done) {
+
+        dataEmailCorrect = {
+            idUsuario: 2,
+            email: "testUser@gmail.com",
+            nombreProducto: "producto10",
+            idProducto: 2,
+            cantidad: 15
+        }
+        dataEmailIncorrect = {
+            idUsuario: 2,
+            email: "testUser+gmail.com",        //daria error por el signo mas.
+            nombreProducto: "producto10",
+            idProducto: 2,
+            cantidad: 15
+        }
+        done();
+    });
+
+    it('Enviar mensaje correcto', (done) => {
+        chai.request('http://localhost:3000')
+            .post('/')
+            .send(dataEmail)
+            .end(function (err, res) {
+                //console.log(res)
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+});
