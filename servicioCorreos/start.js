38b9d3f0b18e7e0ac585594c2ba9c65087495dfd
Carlos Eduardo Cordon Hernandez <carloscordon@Carloss-MacBook-Pro.local>
2020-05-04 23:09:19 -0600

    feat: Mensajeria con colas en contenedor.

--- servicioCorreos/start.js
+++ servicioCorreos/start.js
@@ -1,6 +1,6 @@
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function (error0, connection) {
+amqp.connect('amqp://64.225.24.183:5672', function (error0, connection) {
     if (error0) {
         throw error0;
     }
